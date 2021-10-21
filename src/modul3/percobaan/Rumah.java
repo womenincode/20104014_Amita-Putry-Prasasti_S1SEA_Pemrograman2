697ece8d305316ea624e829d4d576de5e0451add
Amita Putry Prasasti <73166752+womenincode@users.noreply.github.com>
2021-10-21 15:13:18 +0700

    Add modul3

---
+++ src/modul3/percobaan/Rumah.java
@@ -0,0 +1,11 @@
+package modul3.percobaan;
+
+public class Rumah {
+    //atribut
+    String alamat_rumah;
+
+    //method
+    public void setAlamat(String x){
+        alamat_rumah = x;
+    }
+}
