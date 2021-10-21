9146c9c35af918411f6e64138815bb8c81abf14b
Amita Putry Prasasti <73166752+womenincode@users.noreply.github.com>
2021-10-21 19:11:15 +0700

    Add

---
+++ src/modul0/percobaan/Tipe.java
@@ -0,0 +1,22 @@
+package modul0.percobaan;
+
+public class Tipe {
+    public static void main(String[] args) {
+        long data1 = 767226531;
+        int data2 = 22334541;
+        short data3 = 714;
+        byte data4 = 34;
+        double data5 = 4.357;
+        float data6 = (float) 1.733;
+        char data7 = 'c';
+        boolean data8 = true;
+        System.out.println("Nilai Long " + data1);
+        System.out.println("Nilai int " + data2);
+        System.out.println("Nilai short " + data3);
+        System.out.println("Nilai byte " + data4);
+        System.out.println("Nilai double " + data5);
+        System.out.println("Nilai float " + data6);
+        System.out.println("Nilai char " + data7);
+        System.out.println("Nilai double " + data8);
+    }
+}
