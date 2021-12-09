14b8df0a570aa7a6bad81538c0e45ab8bf8571cd
Amita Putry Prasasti <73166752+womenincode@users.noreply.github.com>
2021-12-09 15:12:25 +0700

    Add

--- src/modul8/percobaan/Child.java
+++ src/modul8/percobaan/Child.java
@@ -2,9 +2,7 @@ package modul8.percobaan;
 
 public class Child extends Parent {
     int x = 10;
-
     public void info() {
         System.out.println("Ini Class Chid");
     }
-
 }
--- src/modul8/percobaan/Parent.java
+++ src/modul8/percobaan/Parent.java
@@ -2,9 +2,7 @@ package modul8.percobaan;
 
 public class Parent {
     int x = 5;
-
     public void info() {
         System.out.println("Ini Class Parent");
     }
-
 }
