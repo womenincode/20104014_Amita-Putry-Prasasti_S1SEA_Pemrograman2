fcbe0318cc48dcc9ad653ebdbb9e99d678454334
Amita Putry Prasasti <73166752+womenincode@users.noreply.github.com>
2021-12-22 13:34:42 +0700

    Add

---
+++ src/modul9/latihan/Latihan3.java
@@ -0,0 +1,19 @@
+package modul9.latihan;
+import javax.swing.*;
+class ExceptionA extends Exception {}
+class ExceptionB extends ExceptionA {}
+class ExceptionC extends ExceptionB {}
+public class Latihan3 {
+    public static void main(String[] args) {
+        try {
+            throw new ExceptionC();
+        } catch (ExceptionA a) {
+            JOptionPane.showMessageDialog( null, a.toString(), "Exception", JOptionPane.INFORMATION_MESSAGE);
+        }
+        try {
+            throw new ExceptionB();
+        } catch (ExceptionA b) {
+            JOptionPane.showMessageDialog(null, b.toString(), "Exception", JOptionPane.INFORMATION_MESSAGE);
+        } System.exit(0);
+    }
+}
